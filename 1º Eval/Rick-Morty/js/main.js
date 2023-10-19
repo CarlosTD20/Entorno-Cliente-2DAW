3bdc41684db8165a5d68e64be179bb41a83dfa32
CarlosTD20 <carlostornero204@gmail.com>
2023-10-19 12:36:26 +0200

    fixed textnode and adding new functionality

--- 1º Eval/Rick-Morty/js/main.js
+++ 1º Eval/Rick-Morty/js/main.js
@@ -25,16 +25,69 @@ personajes.results.forEach(c => {
     characterImg.src= c.image
     //console.log(characterImg)
 
+    let moreinformation = document.createElement("button")
+    moreinformation.className="c-character--moreinformation"
+    let setmoreinformation = document.createTextNode("More Information")
+
     containerCharacter.appendChild(characterName)
     containerCharacter.appendChild(characterId)
     containerCharacter.appendChild(characterImg)
+    containerCharacter.appendChild(setmoreinformation)
 
+    containerCharacter.addEventListener("click", function() {
+        getAllInformation(newID);
+    });
     container.appendChild(containerCharacter)
-    containerCharacter.onclick=()=>getAllInformation(newID)
+
 })
 
 function getAllInformation(newID){
-    alert(newID)
+    let characterInformation = personajes.results.find(p=>p.id == newID)
+    console.log(characterInformation)
+
+    let dialog =document.createElement("dialog")
+    let divCharacter = document.createElement("div")
+    divCharacter.className="c-information-character"
+
+    
+    let Imagecharacter = document.createElement("img")
+    Imagecharacter.src=characterInformation.image
+
+    let nameCharacter = document.createElement("h2")
+    let setname=document.createTextNode("Name:" +characterInformation.name)
+    nameCharacter.appendChild(setname)
+
+    let IDCharacter= document.createElement("p")
+    let setID=document.createTextNode("ID:" + characterInformation.id)
+    IDCharacter.appendChild(setID)
+
+    let genderCharacter=document.createElement("p")
+    let setGender = document.createTextNode("Genero: " + characterInformation.gender)
+    genderCharacter.appendChild(setGender)
+
+    let speciesCharacter = document.createElement("p")
+    let setspecies=document.createTextNode("Especie: " + characterInformation.species)
+    speciesCharacter.appendChild(setspecies)
+
+    let closedialog=document.createElement("button")
+    closedialog.className="c-information-character__close"
+    let setclose = document.createTextNode("Close")
+    closedialog.appendChild(setclose)
+
+    divCharacter.appendChild(Imagecharacter)
+    divCharacter.appendChild(nameCharacter)
+    divCharacter.appendChild(IDCharacter)
+    divCharacter.appendChild(genderCharacter)
+    divCharacter.appendChild(speciesCharacter)
+    divCharacter.appendChild(closedialog)
+
+    dialog.appendChild(divCharacter)
+
+    container.appendChild(dialog).showModal()
+
+    closedialog.addEventListener("click",function (){
+        dialog.close()
+    })
 }
 
 /*
