b2bce6763686d1c7072eb7e9a174a4a868e8a16d
Carlos Tornero <carlostornero204@gmail.com>
2023-11-19 13:33:40 +0100

    peticiones

--- 1º Eval/peticiones/js/fetchPromesas.js
+++ 1º Eval/peticiones/js/fetchPromesas.js
@@ -1,12 +1,12 @@
 let url = 'http://localhost:3000/'
 
 function get(entidad) {
-// Return a new promise.
+    // Return a new promise.
     return new Promise(function(resolve, reject) {
-// Do the usual XHR stuff
-        var req = new XMLHttpRequest()
-        req.open('GET', url+entidad)
-        req.reponseType='json'
+        // Do the usual XHR stuff
+        var req = new XMLHttpRequest();
+        req.open('GET', url+ entidad);
+        req.responseType='json'
         req.onload = function() {
             if (req.status == 200) {
                 resolve(req.response); // Resolve the promise with the response text
@@ -14,15 +14,26 @@ function get(entidad) {
             else {
                 reject(Error(req.statusText)); // HTTP Error reject with statusText
             }
-        }
+        };
         // Handle network errors
         req.onerror = function() {
             reject(Error("Network Error"));
-        }
-
-        req.send();
-    })
+        };
+        req.send(); // Make the request
+    });
 }
 
-get("peliculas").then(peliculas => console.log(peliculas))
-                        .catch(error => console.log(error))
+let entidad = prompt('Dime la entidad')
+get(entidad)
+    .then(respuesta => {
+        respuesta.forEach(r=>console.log(r))
+    })
+    .catch(error => console.log(error))
+
+
+
+// let entidad = prompt('Dime la entidad')
+// get(entidad).then(respuesta => {
+//
+// })
+//     .catch(error => console.log(error))
