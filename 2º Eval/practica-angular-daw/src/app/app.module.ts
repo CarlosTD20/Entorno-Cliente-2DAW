047886301bd32b0655ad4929baa9aa9efbf328dc
Carlos Tornero <carlostornero204@gmail.com>
2023-12-19 10:12:07 +0100

    To way Data Binding/Event Binding

--- 2º Eval/practica-angular-daw/src/app/app.module.ts
+++ 2º Eval/practica-angular-daw/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { ArticuloComponent } from './articulo/articulo.component';
 
@@ -12,7 +14,8 @@ import { ArticuloComponent } from './articulo/articulo.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- 2º Eval/practica-angular-daw/src/app/articulo/articulo.component.ts
+++ 2º Eval/practica-angular-daw/src/app/articulo/articulo.component.ts
@@ -11,13 +11,39 @@ export class ArticuloComponent {
 
   articulos = ARTICULOS
 
-  articulo : Articulo = this.articulos[3]
-  
-  botonActivo : boolean = true;
+  selectedArticulo : Articulo = this.articulos[3]
 
   ngOnInit(){
     console.log('ngOnInit...')
     setTimeout(()=>{alert('BotonActivo')
                   this.botonActivo = false},5000)
+  }
+
+  botonActivo : boolean = true;
+
+  activar() {
+    this.botonActivo = false;
+  }
+
+  desactivar(){
+    this.botonActivo = true;
+  }
+
+  seleccionarArticulo(){
+    let posArticulo : string | null = prompt('Introduce la posición del artículo:...')
+
+    if(posArticulo == null){
+      alert('Error. Articulo no seleccionado')
+    } else {
+      if(isNaN(parseInt(posArticulo))){
+        alert('Error. Selección no númerica')
+      }else{
+        if ((parseInt(posArticulo))>=this.articulos.length || (parseInt(posArticulo))< 0){
+          alert('Error. No existe el artículo')
+        } else{
+          this.selectedArticulo = this.articulos[parseInt(posArticulo)];
+        }
+      }
+    }
   }
 }
