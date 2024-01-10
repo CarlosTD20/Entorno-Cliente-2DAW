a0c0c4e65703e7e77a0a59732ac1461bfc7221df
Carlos Tornero <carlostornero204@gmail.com>
2024-01-10 09:35:48 +0100

    start practice prade e hijo

---
+++ 2º Eval/practica-Padre-Hijo/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CardTrabajadorComponent } from './components/card-trabajador/card-trabajador.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardTrabajadorComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ 2º Eval/practica-Padre-Hijo/src/app/modelos/trabajador.ts
@@ -0,0 +1,7 @@
+export interface trabajador{
+    id: number,
+    votos: number,
+    nombre: String,
+    cargo: String,
+    foto: String
+}
