50e6c5367ce6fd202fb47c40062cf6789d4d1e53
Carlos Tornero <carlostornero204@gmail.com>
2024-02-06 09:27:48 +0100

    preparing environment for the exam

--- 2º Eval/examen-ctornero/src/app/app.module.ts
+++ 2º Eval/examen-ctornero/src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
+
+//Imports
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+
+//Declarations
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
