f3cf93ad7bb3c7054afadfcca6b5d6b60fa2892b
Carlos Tornero <carlostornero204@gmail.com>
2024-02-06 09:30:17 +0100

    added notes

--- 2º Eval/apuntes_profesor/src/app/app.module.ts
+++ 2º Eval/apuntes_profesor/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
+// Imports
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
-
 import { HttpClientModule } from '@angular/common/http';
 
+// Declarations
 import { AppComponent } from './app.component';
 import { Ejemplo1Component } from './ejemplo1/ejemplo1.component';
 import { Ejemplo2Component } from './ejemplo2/ejemplo2.component';
